a605d42f0071e95411b5a79e905a69b1443e3be0
dilgitcloud <dileeponnet@gmail.com>
2021-08-05 11:40:23 +0530

    GaugeChart and a few other changes

--- EnergyMeter_dashapp1.py
+++ EnergyMeter_dashapp1.py
@@ -5,6 +5,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import plotly.graph_objects as go
 
 import pandas as pd
 import json
@@ -53,12 +54,24 @@ fig_current_hist = px.histogram(df, x="Current",title="Current Histogram")
 fig_actp_hist = px.histogram(df, x="ActivePower",title="ActivePower Histogram")
 fig_appp_hist = px.histogram(df, x="ApparentPower",title="ApparentPower Histogram")
 
-mx_vi = max(df["Voltage"]*df["Current"])
+
+mx_vi = round(max(df["Voltage"]*df["Current"]),2)
 
 VI_msg = "My peak V*I is "+str(mx_vi)
 
-curr_mn = df["Current"].mean()
-curr_stdev = df["Current"].std()
+data_of_the_minute = df["ApparentPower"].iloc[n_readings_in_a_min*0:n_readings_in_a_min*1]	
+mn = round(data_of_the_minute.mean(),2)
+
+fig_gauge_peakVI = go.Figure(go.Indicator(
+    mode = "gauge+number",
+    value = mn,
+    domain = {'x': [0, 1], 'y': [0, 1]},
+    title = {'text': "Variation of Average Usage (App-Power) across months"}))
+
+#fig_gauge_peakVI.show()
+
+curr_mn = round(df["Current"].mean(),4)
+curr_stdev = round(df["Current"].std(),4)
 load_variation = "Mean Current is "+str(curr_mn)+" and Standard Deviation of Current is "+str(curr_stdev)
 
 #breakpoint()
@@ -74,7 +87,7 @@ app.layout = html.Div([
     #html.Div(children='''
     #    Dash: A web application framework for Python.
     #'''),
-
+    html.Div(
     dcc.RadioItems(id='Radio-graphselect1',
     options=[
         {'label': 'Current', 'value': 'CURR'},
@@ -82,6 +95,7 @@ app.layout = html.Div([
         {'label': 'ApparentPower', 'value': 'APPP'}
     ],
     value='CURR'
+    ),style={"padding": "10px",'width': 1000}
     ),
 	
     dcc.Graph(
@@ -97,28 +111,60 @@ app.layout = html.Div([
             #html.H1('Measures')
         #])
     dbc.Row([dbc.Col(html.Div([
-    html.Label("Move the slider to know mean apparent power for each minute"),
+    html.Label("Move the slider to know mean apparent power for each month"),
     dcc.Slider(
         id='my-minute-slider',
+	marks={i:'{}st month'.format(i) for i in range(0,int(tm[-1]/60)+1)},
         min=0,
         max=tm[-1]/60,#in minutes
         step=1,
         value=2,
     ),
     html.Div(id='slider-show-selection'),
+
+    dcc.Graph(figure=fig_gauge_peakVI,
+        id='peakVI_gauge'
+    ),
+
     ]),
-    width=4, style={'border': '1px solid'}
+    width={'size': 6, 'offset': 3}, style={'border': '1px solid'}
     ),
     
     ]),
-    dbc.Alert(
+    ],style={'marginBottom': 50, 'marginTop': 25,'textAlign': 'center'}),
+    
+    dbc.Row([dbc.Col(	
+    dbc.Alert([
+    html.Div(
             children=VI_msg,
             id="alert-auto",
-            is_open=True,
+            #is_open=True,
+            style={'textAlign': 'center'}
         ),
-    html.Label(load_variation) 
-    ]) 
-	
+    ]), 
+    	#html.Label(load_variation) 
+    	#]),#style={'marginBottom': 50, 'marginTop': 25,'textAlign': 'center'}),
+    width={'size': 6, 'offset': 3},align="center" 
+    )]),
+
+    dbc.Row([dbc.Col(	
+    dbc.Alert([
+    html.Div(
+            children=load_variation,
+            id="alert-auto2",
+            #is_open=True,
+            style={'textAlign': 'center'}
+        ),
+    ]), 
+    #html.Label(load_variation) 
+    #]),#style={'marginBottom': 50, 'marginTop': 25,'textAlign': 'center'}),
+    width={'size': 6, 'offset': 3},align="center" 
+    )]),
+    
+    #dcc.Graph(figure=fig_gauge_peakVI,
+    #    id='peakVI_gauge'
+    #),
+ 	
     ])
     ])
 ])
@@ -128,20 +174,29 @@ app.layout = html.Div([
 @app.callback(
     Output('curr_actp_appp-graph_time', 'figure'),
     Output('curr_actp_appp-graph_hist', 'figure'),
-    Output('slider-show-selection','children'),	
+    Output('peakVI_gauge','figure'),	
+    #Output('slider-show-selection','children'),	
     Input('Radio-graphselect1', 'value'),
     Input('my-minute-slider', 'value'))
 
 def update_graph(choose_which_graph,minute_count):
     #breakpoint()	
     data_of_the_minute = df["ApparentPower"].iloc[n_readings_in_a_min*minute_count:n_readings_in_a_min*(minute_count+1)]	
-    mn = data_of_the_minute.mean()	
+    mn = round(data_of_the_minute.mean(),2)
+    
+    fig_gauge_peakVI = go.Figure(go.Indicator(
+    mode = "gauge+number",
+    value = mn,
+    domain = {'x': [0, 1], 'y': [0, 1]},
+    title = {'text': "Variation of Average Usage (App-Power) across months"}))
+
+    #fig_gauge_peakVI.add_trace(go.Indicator(value = mn)) 	
     if	choose_which_graph=='CURR':
-        return fig_current_timewise, fig_current_hist,"Mean Apparent power in " + str(minute_count)+ "th minute  is "+str(mn)
+        return fig_current_timewise, fig_current_hist,fig_gauge_peakVI#"Mean Apparent power in " + str(minute_count)+ "th minute  is "+str(mn)
     elif choose_which_graph=='ACTP': 	
-        return fig_actp_timewise, fig_actp_hist,"Mean Apparent power in " + str(minute_count)+ "th minute  is "+str(mn)
+        return fig_actp_timewise, fig_actp_hist,fig_gauge_peakVI#"Mean Apparent power in " + str(minute_count)+ "th minute  is "+str(mn)
     elif choose_which_graph=='APPP': 	
-        return fig_appp_timewise, fig_appp_hist,"Mean Apparent power in " + str(minute_count)+ "th minute  is "+str(mn)
+        return fig_appp_timewise, fig_appp_hist,fig_gauge_peakVI#"Mean Apparent power in " + str(minute_count)+ "th minute  is "+str(mn)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
